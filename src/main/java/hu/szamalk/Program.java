5d8319815a5de5b69921794447302339003f6bb0
Sas Gyula <sas.gyula@dk.akkszalezi.hu>
2024-12-09 14:26:32 +0100

    f1 10

--- src/main/java/hu/szamalk/Program.java
+++ src/main/java/hu/szamalk/Program.java
@@ -2,8 +2,14 @@ package hu.szamalk;
 
 import hu.szamalk.modell.Tabla;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path forras = Paths.get("C:\\Users\\sas.gyula\\IdeaProjects\\SasGyulaDolgozat\\src\\main\\java\\hu\\szamalk\\tablak64.txt");
         Tabla t = new Tabla('#');
         System.out.println("4.feladat: Az üres tábla:");
         System.out.println(t.megjelenit());
@@ -13,5 +19,21 @@ public class Program {
         System.out.println("9.feladat: Üres oszlopok és sorok száma:");
         System.out.println("Oszlopok: " + t.uresOszlopokSzama());
         System.out.println("Sorok: " + t.uresSorokSzama());
+        fajlbaIras(forras);
+    }
+    public static void fajlbaIras(Path forras) throws IOException {
+        String s = "";
+        for(int i = 1; i<65; i++){
+            Tabla t = new Tabla('*');
+            t.elhelyez(i);
+            s += t.megjelenit() + "\n";
+        }
+        try {
+            Files.write(forras, s.getBytes());
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -3,53 +3,54 @@ package hu.szamalk.modell;
 import java.util.Random;
 
 public class Tabla {
-    private Character [][] t;
+    private Character[][] t;
     private Character uresCella;
     private Random rn = new Random();
 
     public Tabla(Character uresCella) {
         t = new Character[8][8];
         this.uresCella = uresCella;
-        feltolt();
-    }
-    private void feltolt(){
-        for(int i = 0; i < t.length; i++){
-            for(int j = 0; j < t[i].length; j++){
-                t[i][j] = uresCella;
-            }
-        }
+        tabla();
     }
 
-    public void elhelyez(int n){
+    public void elhelyez(int n) {
         int rnOszlop;
         int rnSor;
-        for(int i = 0; i < n; i++){
-            rnOszlop = rn.nextInt(0,8);
-            rnSor = rn.nextInt(0,8);
+        for (int i = 0; i < n; i++) {
+            rnOszlop = rn.nextInt(0, 8);
+            rnSor = rn.nextInt(0, 8);
+            while(this.t[rnSor][rnOszlop] == 'K'){
+                rnOszlop = rn.nextInt(0, 8);
+                rnSor = rn.nextInt(0, 8);
+            }
             this.t[rnSor][rnOszlop] = 'K';
         }
     }
-    public void fajlbaIr(){
 
-    }
-    public String megjelenit(){
+    public String megjelenit() {
         String s = "";
-        for(int i = 0; i < t.length; i++){
-            for(int j = 0; j < t[i].length; j++){
-                if(j % 8 == 0){
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++) {
+                if (j % 8 == 0) {
                     s += '\n';
                     s += t[i][j];
-                }else {
+                } else {
                     s += t[i][j];
                 }
             }
         }
         return s;
     }
-    public void tabla(){
 
+    public void tabla() {
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++) {
+                t[i][j] = uresCella;
+            }
+        }
     }
-    public boolean uresSor(int sor){
+
+    public boolean uresSor(int sor) {
         boolean s = true;
         int i = 0;
         while (i < t[sor].length) {
@@ -61,7 +62,8 @@ public class Tabla {
         }
         return s;
     }
-    public boolean uresOszlop(int oszlop){
+
+    public boolean uresOszlop(int oszlop) {
         boolean s = true;
         int i = 0;
         while (i < t.length) {
@@ -73,22 +75,29 @@ public class Tabla {
         }
         return s;
     }
-    public int uresOszlopokSzama(){
+
+    public int uresOszlopokSzama() {
         int s = 0;
-        for(int i = 0; i < t[0].length; i++){
-            if(uresOszlop(i) == true){
+        for (int i = 0; i < t[0].length; i++) {
+            if (uresOszlop(i) == true) {
                 s++;
             }
         }
         return s;
     }
-    public int uresSorokSzama(){
+
+    public int uresSorokSzama() {
         int s = 0;
-        for(int i = 0; i < t.length; i++){
-            if(uresSor(i) == true){
+        for (int i = 0; i < t.length; i++) {
+            if (uresSor(i) == true) {
                 s++;
             }
         }
         return s;
     }
+
+    public void fajlbaIr() {
+
+    }
+
 }
