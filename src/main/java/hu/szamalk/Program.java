f050667ce48cd63a6d7f5c5e4cc19c97ce292da3
Sas Gyula <sas.gyula@dk.akkszalezi.hu>
2024-12-09 13:43:09 +0100

    f1 6

--- src/main/java/hu/szamalk/Program.java
+++ src/main/java/hu/szamalk/Program.java
@@ -5,8 +5,10 @@ import hu.szamalk.modell.Tabla;
 public class Program {
     public static void main(String[] args) {
         Tabla t = new Tabla('#');
+        System.out.println("4.feladat:");
         System.out.println(t.megjelenit());
-        t.elhelyez(2);
+        t.elhelyez(8);
+        System.out.println("6.feladat:");
         System.out.println(t.megjelenit());
     }
 }
