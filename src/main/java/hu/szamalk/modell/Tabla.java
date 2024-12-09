9eb364d8bf8ffd48182c2a02ddd0a1ea9214f9e5
Sas Gyula <sas.gyula@dk.akkszalezi.hu>
2024-12-09 13:25:48 +0100

    f1 1

---
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -0,0 +1,29 @@
+package hu.szamalk.modell;
+
+public class Tabla {
+
+    public void elhelyez(){
+
+    }
+    public void fajlbaIr(){
+
+    }
+    public void megjelenit(){
+
+    }
+    public void tabla(){
+
+    }
+    public void uresOszlop(){
+
+    }
+    public void uresSor(){
+
+    }
+    public int uresOszlopokSzama(){
+        return 0;
+    }
+    public int uresSorokSzama(){
+        return 0;
+    }
+}
