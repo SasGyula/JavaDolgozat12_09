4a74e0e01b6333661cf825e375aa64ad4d0ed1fa
Sas Gyula <sas.gyula@dk.akkszalezi.hu>
2025-02-17 14:48:27 +0100

    test results

--- src/test/java/hu/ruander/ProgramTest.java
+++ src/test/java/hu/ruander/ProgramTest.java
@@ -36,11 +36,11 @@ class ProgramTest {
     @Test
     void testSzamolasok(){
         Assertions.assertEquals(5.96, program.osszSuly(), 0.01);
-//        Assertions.assertEquals(4.81, program.gombokOsszSuly(), 0.01);
-//
-//        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
-//        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
-//        Gomb gomb = new Gomb(1.0);
-//        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
+        Assertions.assertEquals(4.81, program.gombokOsszSuly(), 0.01);
+
+        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
+        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
+        Gomb gomb = new Gomb(1.0);
+        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
     }
 }
